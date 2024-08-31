88bcd810d927282c92996d8226ff6241f5f2bd5c
Binayak Mondal <76868488+binayak9932@users.noreply.github.com>
2024-09-01 04:36:38 +0530

    Update app.py

--- emotion_detect/app.py
+++ emotion_detect/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/analyze', methods=['POST'])
 def analyze_image():
-    # Check if the post request has the file part
+    
     if 'file' not in request.files:
         return jsonify({"error": "No file part"}), 400
 
@@ -17,14 +17,14 @@ def analyze_image():
         return jsonify({"error": "No selected file"}), 400
 
     if file:
-        # Save the file temporarily
+        
         filename = "temp.jpg"
         file.save(filename)
 
         try:
-            # Perform facial attribute analysis
+            
             analysis = DeepFace.analyze(img_path=filename, actions=['age', 'gender', 'emotion'])
-            # Clean up the saved file
+           
             os.remove(filename)
             return jsonify(analysis)
         except Exception as e:
